89851c9cab96eafc26e54cdde1117c1a520e2fbb
Ben Schultz <bens4lsu@gmail.com>
2021-11-04 15:58:35 -0500

    converted routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -19,7 +19,7 @@ public func routes(_ app: Application) throws {
     
     // MARK:  Miscellaneous Routes
     
-    app.get { req throws -> EventLoopFuture<Response> in
+    app.get { req async throws -> Response in
         
         struct IndexContext: Encodable, ResponseEncodable, Content {
             func encodeResponse(for request: Request) -> EventLoopFuture<Response> {
@@ -34,11 +34,11 @@ public func routes(_ app: Application) throws {
             var accessDictionary: [String: Bool]
         }
         
-        return try UserAndTokenController.verifyAccess(req, accessLevel: .activeOnly) { user in
+        return try await UserAndTokenController.ifVerifiedDo(req, accessLevel: .activeOnly) { user in
             let version = Version().version
             let accessDictionary = user.accessDictionary()
             let context = IndexContext(version: version, accessDictionary: accessDictionary)
-            return req.view.render("index", context).encodeResponse(for: req)
+            return try await req.view.render("index", context).encodeResponse(for: req)
         }
     }
     
@@ -48,23 +48,21 @@ public func routes(_ app: Application) throws {
         let version: String
     }
         
-    app.get("sql") { req -> EventLoopFuture<String> in
+    app.get("sql") { req async throws -> String in
         let queryString = SQLQueryString(stringLiteral: "SELECT @@version as version")
-        return (req.db as! SQLDatabase).raw(queryString).first(decoding: MySQLVersion.self).map { mySqlVersion in
-            return mySqlVersion?.version ?? "error retrieving mysql version"
-        }
+        let mySqlVersion = try await (req.db as! SQLDatabase).raw(queryString).first(decoding: MySQLVersion.self).get()
+        return mySqlVersion?.version ?? "error retrieving mysql version"
     }
     
     
-    app.get("blankpage") { req in
-        return req.eventLoop.makeSucceededFuture("")
+    app.get("blankpage") { req async throws in
+        ("")
     }
     
     
-    app.get("clearcache") { req in
-        return req.eventLoop.makeSucceededFuture(cache.clear()).map() {
-            return req.eventLoop.makeSucceededFuture("Caches Cleard.")
-        }
+    app.get("clearcache") { req async throws -> String in
+        cache.clear()
+        return "Caches Cleard."
     }
 
 }
